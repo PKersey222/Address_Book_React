1b0fbdc80e33e2e934523f6f8304d3ef95147607
Philip-Kersey <8fullhouse8@gmail.com>
2022-08-15 18:34:48 -0500

    final code

--- components/AddressDetails.jsx
+++ components/AddressDetails.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
 
-function BeerDetails(props) {
-  const [isClicked, setIsClicked] = useState(false);
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-    console.log(isClicked);
+export default function AddressDetails(props) {
+  const [moreInfo, setMoreInfo] = useState(false);
+  const MoreDetails = () => {
+    return (
+      <div>
+        <p>gender:{props.user.gender}</p>
+        <p>DOB:{props.user.date}</p>
+        <p>Age:{props.user.age}</p>
+        <p>
+          Location:{props.user.location.street.number}
+          {props.user.location.street.name}
+          {props.user.location.city}
+          {props.user.location.state}
+        </p>
+      </div>
+    );
   };
-
-  console.log(props);
-  const { beer } = props;
+  
   return (
     <div>
-      <img className="beer_image" src={beer.image_url} />
-      <p>{beer.name}</p>
-      {isClicked ? (
-        <button onClick={handleClick}>Unlike</button>
-      ) : (
-        <button onClick={handleClick}>Like</button>
-      )}
-    </div>
-  );
+    {moreInfo && <MoreDetails />}
+    <button onClick={() => setMoreInfo(!moreInfo)}>
+      {moreInfo ? "hide details" : "show details"}
+    </button>
+  </div>
+);
 }
 
-export default BeerDetails;
